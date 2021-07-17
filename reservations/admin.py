6d07cf5f118bb346bfe7eaf1c99c81ba736977f1
Mohamed Omar <m.jiboman@gmail.com>
2021-07-17 18:27:25 +0300

    models creation finished

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.reservation)
+class reservationAdmin(admin.ModelAdmin):
+
+    """Reservation Admin Definition"""
+
+    pass
